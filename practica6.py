a8b3aa298f04bddaeced9b03dceb2555ff342554
DanielaBarrios <103336142+DanielaBarrios@users.noreply.github.com>
2022-05-16 01:16:12 -0500

    Update practica6.py

--- practica6.py
+++ practica6.py
@@ -3,3 +3,35 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math as ma
 import cv2 #opencv
+
+##practica 6
+import numpy as np
+from matplotlib import pyplot as plt
+import math as ma
+import cv2 #opencv
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+
+    lower_red = np.array([50,150,30])
+    upper_red = np.array([180,255,255])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
